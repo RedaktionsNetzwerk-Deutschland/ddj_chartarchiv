b6781c32bf5ab46bd85e82bcd705b6e316820eb6
Your Name <you@example.com>
2025-05-15 15:23:52 +0200

    Language-Code für Debug-Ausgabe geändert

--- archive_webapp/settings.py
+++ archive_webapp/settings.py
@@ -156,8 +156,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'de-de'
-
+#LANGUAGE_CODE = 'de-de'
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
